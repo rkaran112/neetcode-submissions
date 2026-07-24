5f73b8cde293ea4189378488cca370408ab48c09
Karan <155136413+rkaran112@users.noreply.github.com>
2026-07-24 18:46:10 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        seen = {}
+        teen = {}
+        if len(s)!= len(t):
+            return False
+        for i in s:
+            seen[i] = seen.get(i,0)+1
+        for j in t:
+            teen[j] = teen.get(j,0)+1
+        return seen ==teen
+
+
+        
